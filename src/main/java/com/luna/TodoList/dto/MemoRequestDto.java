65949ec82ec55274fc40c5c000e9eb9b872bfa42
LunaTW <68732727+LunaTW@users.noreply.github.com>
2020-12-28 18:27:31 +0800

    fix: correct import @Notnull

--- src/main/java/com/luna/TodoList/dto/MemoRequestDto.java
+++ src/main/java/com/luna/TodoList/dto/MemoRequestDto.java
@@ -1,9 +1,10 @@
 package com.luna.TodoList.dto;
 
-import com.sun.istack.internal.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
 
 import static java.lang.Boolean.FALSE;
 
